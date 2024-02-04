f59a77b76cd4178636873e60f932d5f0c2d860bc
Abimael Tavares <tavares@abimael.dev.br>
2024-02-04 01:02:26 -0300

    fix result

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -2,64 +2,66 @@
 
 /* Profile Object  */
 
-const myProfile = {};
+window.onload = () => {
+  const myProfile = {};
 
-/* Populate Profile Object with placesLive objects */
+  /* Populate Profile Object with placesLive objects */
 
-myProfile.name = "Abimael S. Tavares";
-myProfile.photo = "images/me.JPG";
-myProfile.favoriteFoods = ["pasta", "banana", "orange", "apple", "bread"];
-myProfile.hobbies = ["study", "jiu-jitsu", "swimming"];
-myProfile.placesLived = [];
+  myProfile.name = "Abimael S. Tavares";
+  myProfile.photo = "images/me.JPG";
+  myProfile.favoriteFoods = ["pasta", "banana", "orange", "apple", "bread"];
+  myProfile.hobbies = ["study", "jiu-jitsu", "swimming"];
+  myProfile.placesLived = [];
 
-myProfile.placesLived.push({ place: "Fortaleza, CE", length: "35 Years" });
-myProfile.placesLived.push({ place: "Iguatu, CE", length: "3 Years" });
+  myProfile.placesLived.push({ place: "Fortaleza, CE", length: "35 Years" });
+  myProfile.placesLived.push({ place: "Iguatu, CE", length: "3 Years" });
 
-/* DOM Manipulation - Output */
+  /* DOM Manipulation - Output */
 
-const { name, photo, favoriteFoods, hobbies, placesLived } = myProfile;
+  const { name, photo, favoriteFoods, hobbies, placesLived } = myProfile;
 
-/* Name */
+  /* Name */
 
-document.querySelector("#name").textContent = name;
+  document.querySelector("#name").textContent = name;
 
-/* Photo with attributes */
+  /* Photo with attributes */
 
-const photoElement = document.querySelector("#photo");
+  const photoElement = document.querySelector("#photo");
 
-photoElement.setAttribute("src", photo);
-photoElement.setAttribute("alt", name);
+  photoElement.setAttribute("src", photo);
+  photoElement.setAttribute("alt", name);
 
-/* Favorite Foods List*/
+  /* Favorite Foods List*/
 
-const favoriteFoodsUl = document.querySelector("#favorite-foods");
+  const favoriteFoodsUl = document.querySelector("#favorite-foods");
 
-favoriteFoods.forEach((food) => {
-  const li = document.createElement("li");
-  li.textContent = food;
-  favoriteFoodsUl.appendChild(li);
-});
+  favoriteFoods.forEach((food) => {
+    const li = document.createElement("li");
+    li.textContent = food;
+    favoriteFoodsUl.appendChild(li);
+  });
 
-/* Hobbies List */
+  /* Hobbies List */
 
-const hobbiesUl = document.querySelector("#hobbies");
+  const hobbiesUl = document.querySelector("#hobbies");
 
-hobbies.forEach((hobbie) => {
-  const li = document.createElement("li");
-  li.textContent = hobbie;
-  hobbiesUl.appendChild(li);
-});
+  hobbies.forEach((hobbie) => {
+    const li = document.createElement("li");
+    li.textContent = hobbie;
+    hobbiesUl.appendChild(li);
+  });
 
-/* Places Lived DataList */
-const placesLivedEl = document.querySelector("#places-lived");
+  /* Places Lived DataList */
+  const placesLivedEl = document.querySelector("#places-lived");
 
-placesLived.forEach(({ place, length }) => {
-  const dt = document.createElement("dt");
-  const dd = document.createElement("dd");
+  placesLived.forEach(({ place, length }) => {
+    const dt = document.createElement("dt");
+    const dd = document.createElement("dd");
 
-  dt.textContent = place;
-  dd.textContent = length;
+    dt.textContent = place;
+    dd.textContent = length;
 
-  placesLivedEl.appendChild(dt);
-  placesLivedEl.appendChild(dd);
-});
+    placesLivedEl.appendChild(dt);
+    placesLivedEl.appendChild(dd);
+  });
+};
